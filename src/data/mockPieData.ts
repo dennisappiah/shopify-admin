751038cdf39526275f8b7e9c173f568b9a7800ea
dennisappiah <appiahdennis1998@gmail.com>
2024-05-28 11:08:22 +0000

    data

---
+++ src/data/mockPieData.ts
@@ -0,0 +1,32 @@
+export const mockPieData = [
+  {
+    id: "haskell",
+    label: "haskell",
+    value: 124,
+    color: "hsl(333, 70%, 50%)",
+  },
+  {
+    id: "java",
+    label: "java",
+    value: 207,
+    color: "hsl(336, 70%, 50%)",
+  },
+  {
+    id: "hack",
+    label: "hack",
+    value: 219,
+    color: "hsl(165, 70%, 50%)",
+  },
+  {
+    id: "rust",
+    label: "rust",
+    value: 64,
+    color: "hsl(265, 70%, 50%)",
+  },
+  {
+    id: "javascript",
+    label: "javascript",
+    value: 99,
+    color: "hsl(199, 70%, 50%)",
+  },
+];
